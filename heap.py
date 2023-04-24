971303e7681369ebad8d06c289e691e2aeddd068
Shaked Oren <shakedoren957@gmail.com>
2023-04-25 00:16:04 +0300

    add bad bild heap

--- heap.py
+++ heap.py
@@ -1,3 +1,5 @@
+import math
+
 class MaxMinHeap:
     def __init__(self, data=None):
         if data is None:
@@ -8,7 +10,7 @@ class MaxMinHeap:
     def parent(self, i):
         if(i-1 < 0):
             return -1
-        return (i - 1) / 2
+        return math.floor((i - 1) / 2)
 
     def left(self, i):
         return 2 * i + 1
@@ -19,7 +21,7 @@ class MaxMinHeap:
     def size(self):
         return len(self.data)
 
-    def _swap(self, i, j):
+    def swap(self, i, j):
         self.data[i], self.data[j] = self.data[j], self.data[i]
 
     def print_heap(self):
@@ -61,3 +63,32 @@ class MaxMinHeap:
         zipped_lines = zip(left, right)
         lines = [first_line, second_line] + [a + u * ' ' + b for a, b in zipped_lines]
         return lines, n + m + u, max(p, q) + 2, n + u // 2
+
+    def height(self, index):
+        if index == 0:
+            return 0
+
+        return math.floor(math.log2(index + 1))
+
+    def build_heap(self): 
+        for i in range(len(self.data) - 1, math.ceil((len(self.data) - 1) / 2), -1):
+            self.heapify_node(i)
+
+    def build_heap2(self): 
+        for i in range(len(self.data) - 1, 0, -1):
+            self.heapify_node(i)
+
+    def heapify_node(self, index):
+        current_parent_index = index
+        while self.parent(current_parent_index) != -1:
+            current_parent_index = self.parent(current_parent_index)
+            if self.height(current_parent_index) % 2 == 0:
+                if self.data[index] > self.data[current_parent_index]:
+                    self.swap(index, current_parent_index)
+                    self.heapify_node(index)
+                    index = current_parent_index
+            else:
+                if self.data[index] < self.data[current_parent_index]:
+                    self.swap(index, current_parent_index)
+                    self.heapify_node(index)
+                    index = current_parent_index
--- main.py
+++ main.py
@@ -3,17 +3,22 @@ from heap import MaxMinHeap
 
 def main():
   # Create a max min heap.
-  heap = MaxMinHeap([0, 1, 2, 3, 487666, 5, 34, 456, 38, 245, 846, 8469 ,222, 333])
+  heap = MaxMinHeap([
+0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100
+  ])
 
   # Check the size of the heap.
-  print("The size of the heap is", heap.size())
+ # print("The size of the heap is", heap.size())
   # Print the heap.
+  
+  heap.build_heap()
   heap.print_heap()
 
+
   # Check the left, right and parent of the root node.
-  print("The left child of the root node is", heap.left(0))
-  print("The right child of the root node is", heap.right(0))
-  print("The parent of the root node is", heap.parent(0))
+#  print("The left child of the root node is", heap.left(0))
+#  print("The right child of the root node is", heap.right(0))
+#  print("The parent of the root node is", heap.parent(0))
 
 if __name__ == "__main__":
   main()
