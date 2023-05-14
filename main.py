3bab40a3193df8e41e8a75f47c1c1bd6e5e045e0
Roey Shtern <roeyshtern@gmail.com>
2023-05-14 20:42:31 +0300

    changes

--- main.py
+++ main.py
@@ -7,17 +7,17 @@ from menu import start
 
 def main():
     #test_randoms()
-    star    t()
+    start()
 
 
-def test_randoms():
-    for i in range(1000):
-        heap = MaxMinHeap(get_values_from_file(f'tests/test_{i}'))
-        heap.build_heap()
-        num_of_ops = random.randint(1, 3000)
-        for _ in range(num_of_ops):
-            heap.do_random_op()
-            assert check_if_heap_is_good(heap)
+# def test_randoms():
+#     for i in range(1000):
+#         heap = MaxMinHeap(get_values_from_file(f'tests/test_{i}'))
+#         heap.build_heap()
+#         num_of_ops = random.randint(1, 3000)
+#         for _ in range(num_of_ops):
+#             heap.do_random_op()
+#             assert check_if_heap_is_good(heap)
 
 
 if __name__ == '__main__':
